d01d4c2aaa41cdc3cce39ca2f20941a88437c4a4
付军军 <1045245697@qq.com>
2017-02-08 19:21:53 +0800

    drag

---
+++ drag/js/index.js
@@ -0,0 +1,25 @@
+$(window).ready(function () {
+  var box = document.querySelector("div.box");//获取元素
+  var top0 = 0,
+      left0 = 0;//声明初始的top和left值
+  touch.on(box,"touchstart",function (ev) {
+    ev.preventDefault();
+  });//touchstart
+  touch.on(box,"drag",function (ev) {
+    ev.preventDefault();
+    this.style.cssText = "top:"+(ev.y+top0)+"px;left:"+(ev.x+left0)+"px;"
+  });//drag
+  touch.on(box,"dragend",function (ev) {
+    top0 = parseInt(this.style.top);
+    left0 = parseInt(this.style.left);
+    console.log(top0,left0);
+  });//dragend
+  console.log(window);
+
+
+
+
+
+
+
+});//dom加载完成
